1272f0c8846cc9635d5ca90f132ff0c08e11130d
Anil khand <anilkhandthakuri@gmail.com>
2026-03-03 17:56:15 +1100

    Working on Contact Form

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -17,10 +17,10 @@ const LocationMap = () => {
                         <h3 className="map-card-title">Navadurga Sanitaryware</h3>
                         <div className="map-card-address">
                             <MapPin size={20} className="map-icon" />
-                            <span>Srijana Chowk, Pokhara,<br />Gandaki Province, Nepal</span>
+                            <span>Aalu Maila Chowk, Pokhara,<br />Gandaki Province, Nepal</span>
                         </div>
                         <a
-                            href="https://maps.google.com/?q=Pokhara,Nepal"
+                            href="https://www.google.com/maps/place/Navadurga+Sanitaryware+and+Hardware/@28.2177905,83.9837224,17z/data=!4m16!1m9!3m8!1s0x3995954091ecd0cd:0x8cf24278bd6d3e6e!2sNavadurga+Sanitaryware+and+Hardware!8m2!3d28.2177905!4d83.9837224!9m1!1b1!16s%2Fg%2F11rr5jt0hw!3m5!1s0x3995954091ecd0cd:0x8cf24278bd6d3e6e!8m2!3d28.2177905!4d83.9837224!16s%2Fg%2F11rr5jt0hw?entry=ttu"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="btn btn-primary map-btn"
@@ -31,7 +31,7 @@ const LocationMap = () => {
 
                     <div className="google-map-embed">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112467.24157158309!2d83.9016183!3d28.2201994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995937bbf0376ff%3A0xf6cf823b25803e7c!2sPokhara%2C%20Nepal!5e0!3m2!1sen!2sus!4v1700000000000!5m2!1sen!2sus"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3515.688846985023!2d83.98114747514101!3d28.21779517589304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995954091ecd0cd%3A0x8cf24278bd6d3e6e!2sNavadurga%20Sanitaryware%20and%20Hardware!5e0!3m2!1sen!2snp!4v1700000000000!5m2!1sen!2snp"
                             width="100%"
                             height="100%"
                             style={{ border: 0 }}
